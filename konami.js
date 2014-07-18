90497df93094888cc23b9abac90632b825d7e9c5
shinichi takahashi <shinichi_takahashi@lockon.co.jp>
2014-07-18 16:22:35 +0900

    first commit

---
+++ konami.js
@@ -0,0 +1,22 @@
+;(function($){
+	$.fn.konami = (function(styles, duration, easing, callback){
+		var $this = $(this),
+			input = [],
+			command = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+		input = command.slice(0);
+
+		$(window).on("keyup", function(e){
+			var key = e.keyCode;
+			
+			if (key == input[0]) {
+				input.shift();
+				
+				if (input.length == 0) {
+					$this.animate(styles, duration, easing, callback);
+				}
+			} else {
+				input = command.slice(0);
+			}
+		});
+	});
+})(jQuery);
